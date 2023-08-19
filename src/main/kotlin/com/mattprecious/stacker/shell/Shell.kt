3b417b0473d6b8562b2c9cbba3f58f41f9b2912b
Matthew Precious <mattprecious@gmail.com>
2023-08-19 09:12:33 -0400

    Simplify Shell API now that it's mostly unused

--- src/main/kotlin/com/mattprecious/stacker/shell/Shell.kt
+++ src/main/kotlin/com/mattprecious/stacker/shell/Shell.kt
@@ -1,17 +1,8 @@
 package com.mattprecious.stacker.shell
 
-import okio.Source
-
 interface Shell {
 	fun exec(
 		command: String,
 		vararg args: String,
-		input: Source? = null,
-		suppressErrors: Boolean = false,
 	): String
-
-	fun execStatus(
-		command: String,
-		vararg args: String,
-	): Boolean
 }
