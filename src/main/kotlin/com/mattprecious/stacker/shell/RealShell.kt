996c89834138c0fa8b2d780e4c08707c368ab380
Matthew Precious <mattprecious@gmail.com>
2023-08-19 09:12:33 -0400

    Simplify Shell API now that it's mostly unused

--- src/main/kotlin/com/mattprecious/stacker/shell/RealShell.kt
+++ src/main/kotlin/com/mattprecious/stacker/shell/RealShell.kt
@@ -1,8 +1,6 @@
 package com.mattprecious.stacker.shell
 
-import okio.Source
 import okio.buffer
-import okio.sink
 import okio.source
 import java.util.concurrent.TimeUnit
 
@@ -10,41 +8,8 @@ class RealShell : Shell {
 	override fun exec(
 		command: String,
 		vararg args: String,
-		input: Source?,
-		suppressErrors: Boolean,
 	): String {
 		val process = ProcessBuilder(command, *args).run {
-			if (suppressErrors) {
-				redirectError(ProcessBuilder.Redirect.DISCARD)
-			} else {
-				redirectError(ProcessBuilder.Redirect.INHERIT)
-			}
-		}.start()
-
-		if (input != null) {
-			process.outputStream.sink().buffer().use { it.writeAll(input) }
-		}
-
-		check(process.waitFor(20, TimeUnit.SECONDS)) {
-			"Process $command took more than 20 seconds"
-		}
-
-		if (!suppressErrors) {
-			val exitValue = process.exitValue()
-			check(exitValue == 0) {
-				"Process $command exited: $exitValue"
-			}
-		}
-
-		return process.inputStream.source().buffer().use { it.readUtf8().trim() }
-	}
-
-	override fun execStatus(
-		command: String,
-		vararg args: String,
-	): Boolean {
-		val process = ProcessBuilder(command, *args).run {
-			redirectOutput(ProcessBuilder.Redirect.INHERIT)
 			redirectError(ProcessBuilder.Redirect.INHERIT)
 		}.start()
 
@@ -52,6 +17,11 @@ class RealShell : Shell {
 			"Process $command took more than 20 seconds"
 		}
 
-		return process.exitValue() == 0
+		val exitValue = process.exitValue()
+		check(exitValue == 0) {
+			"Process $command exited: $exitValue"
+		}
+
+		return process.inputStream.source().buffer().use { it.readUtf8().trim() }
 	}
 }
