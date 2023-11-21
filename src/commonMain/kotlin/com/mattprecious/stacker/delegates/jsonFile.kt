2e205dbf83b90b77248e0f8664b673861e3f56ff
Matthew Precious <mattprecious@gmail.com>
2023-11-21 16:16:46 -0500

    File perms

--- src/commonMain/kotlin/com/mattprecious/stacker/delegates/jsonFile.kt
+++ src/commonMain/kotlin/com/mattprecious/stacker/delegates/jsonFile.kt
@@ -1,5 +1,8 @@
 package com.mattprecious.stacker.delegates
 
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
@@ -7,6 +10,8 @@ import okio.FileSystem
 import okio.Path
 import okio.buffer
 import okio.use
+import platform.posix.chmod
+import platform.posix.stat
 import kotlin.reflect.KProperty
 import kotlin.reflect.KType
 import kotlin.reflect.typeOf
@@ -32,6 +37,7 @@ class JsonFileDelegate<T : Any?>(
 			is Optional.Some -> v.value
 			is Optional.None -> {
 				val loadedValue = if (fs.exists(path)) {
+					path.requireRestrictiveAccess()
 					val json = fs.source(path).buffer().use { it.readUtf8() }
 					Json.decodeFromString(serializer, json)
 				} else {
@@ -48,7 +54,29 @@ class JsonFileDelegate<T : Any?>(
 	operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
 		fs.createDirectories(path.parent!!)
 		fs.sink(path).buffer().use { it.writeUtf8(Json.encodeToString(serializer, value)) }
+		path.setRestrictiveAccess()
 
 		this.value = Optional.Some(value)
+	}
+
+	private fun Path.setRestrictiveAccess() {
+		// TODO: This won't work on Windows and assumes that the path root is system root.
+		chmod(this.toString(), 256.toUShort())
+	}
+
+	private fun Path.requireRestrictiveAccess() {
+		// TODO: This won't work on Windows and assumes that the path root is system root.
+		memScoped {
+			val path = this@requireRestrictiveAccess
+
+			val stat = alloc<stat>()
+			stat(path.toString(), stat.ptr)
+
+			// The last nine bits are the user, group, and 'other' access permissions, with 3 bits allocated to each.
+			// We require the mode to be 400 (octal), which is 256 in decimal.
+			check(stat.st_mode.toInt() and 0b111111111 == 256) {
+				"User configuration file access is too permissive. Please set file mode of $path to 0400."
+			}
+		}
 	}
 }
