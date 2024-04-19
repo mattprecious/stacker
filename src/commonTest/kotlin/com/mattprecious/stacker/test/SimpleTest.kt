902bb183ba5051a6f564edc1e54b3e9c79a6c652
Matthew Precious <mattprecious@gmail.com>
2024-04-18 22:16:31 -0400

    Configure tests for deterministic git SHAs (#131)

--- src/commonTest/kotlin/com/mattprecious/stacker/test/SimpleTest.kt
+++ src/commonTest/kotlin/com/mattprecious/stacker/test/SimpleTest.kt
@@ -11,8 +11,21 @@ class SimpleTest {
 	fun returnsErrorIfNoRepositoryFound() {
 		stackerTest {
 			// TODO: Assert against the message that's printed as well.
-			assertThat(run()).isEqualTo(-1)
+			assertThat(runStacker()).isEqualTo(-1)
 			assertThat(fileSystem.list("/".toPath())).isEmpty()
+		}
+	}
+
+	@Test
+	fun environmentSetupMakesGitShaDeterministic() {
+		stackerTest {
+			// TODO: Introduce helpers for all of these git operations.
+			environment.exec("git init")
+			environment.exec("touch hello.txt")
+			environment.exec("git add hello.txt")
+			environment.exec("git commit -m 'Testing'")
+			assertThat(environment.exec("git rev-parse HEAD"))
+				.isEqualTo("f8cdffa9a5c120b21a0042138806a930e72af88f")
 		}
 	}
 }
--- src/commonTest/kotlin/com/mattprecious/stacker/test/stackerTest.kt
+++ src/commonTest/kotlin/com/mattprecious/stacker/test/stackerTest.kt
@@ -4,6 +4,7 @@ import com.mattprecious.stacker.remote.FakeRemote
 import com.mattprecious.stacker.withStacker
 import kotlinx.cinterop.convert
 import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.refTo
 import kotlinx.cinterop.toKString
 import okio.ByteString.Companion.toByteString
 import okio.FileSystem
@@ -11,28 +12,34 @@ import okio.ForwardingFileSystem
 import okio.Path
 import okio.Path.Companion.toPath
 import platform.posix.chdir
+import platform.posix.fgets
 import platform.posix.getcwd
+import platform.posix.getenv
+import platform.posix.pclose
+import platform.posix.popen
+import platform.posix.setenv
+import platform.posix.unsetenv
 import kotlin.random.Random
+import kotlin.reflect.KProperty
+import kotlin.test.fail
 
 fun stackerTest(
 	validate: StackerTestScope.() -> Unit,
 ) {
+	val environment = Environment()
 	val fileSystem: FileSystem = TempFileSystem("StackerTest")
 	val root = "/".toPath()
 
 	try {
 		fileSystem.createDirectories(root, mustCreate = true)
 
-		// Grab the current directory so that we can change back before exiting.
-		val startingDirectory = memScoped { getcwd(null, 0.convert())!!.toKString() }
-		try {
-			// Note: This chdir is only necessary because we're currently shelling out for git push/pull. If that moves back
-			// to using libgit then we can remove this and rely only on the wrapped file system.
-			chdir(fileSystem.canonicalize(root).toString())
+		environment.withSnapshot {
+			environment.workingDirectory = fileSystem.canonicalize(root).toString()
+			environment.setGitNames("Stacker")
+			environment.setGitEmails("stacker@example.com")
+			environment.setGitDates("2020-01-01T12:00:00Z")
 
-			StackerTestScope(fileSystem).validate()
-		} finally {
-			chdir(startingDirectory)
+			StackerTestScope(environment, fileSystem).validate()
 		}
 	} finally {
 		fileSystem.deleteRecursively(root)
@@ -40,11 +47,12 @@ fun stackerTest(
 }
 
 class StackerTestScope(
+	val environment: Environment,
 	val fileSystem: FileSystem,
 ) {
 	val remote = FakeRemote()
 
-	fun run(vararg args: String): Int {
+	fun runStacker(vararg args: String): Int {
 		return withStacker(
 			fileSystem = fileSystem,
 			remoteOverride = remote,
@@ -52,6 +60,109 @@ class StackerTestScope(
 			it.parse(args.asList())
 		}
 	}
+}
+
+class Environment {
+	var workingDirectory: String
+		get() = memScoped { getcwd(null, 0.convert())!!.toKString() }
+		set(value) = check(chdir(value) == 0)
+
+	var gitAuthorDate: String? by Variable("GIT_AUTHOR_DATE")
+	var gitAuthorEmail: String? by Variable("GIT_AUTHOR_EMAIL")
+	var gitAuthorName: String? by Variable("GIT_AUTHOR_NAME")
+	var gitCommitterDate: String? by Variable("GIT_COMMITTER_DATE")
+	var gitCommitterEmail: String? by Variable("GIT_COMMITTER_EMAIL")
+	var gitCommitterName: String? by Variable("GIT_COMMITTER_NAME")
+
+	fun setGitDates(date: String) {
+		gitAuthorDate = date
+		gitCommitterDate = date
+	}
+
+	fun setGitEmails(email: String) {
+		gitAuthorEmail = email
+		gitCommitterEmail = email
+	}
+
+	fun setGitNames(name: String) {
+		gitAuthorName = name
+		gitCommitterName = name
+	}
+
+	/** Executes [command] and returns both the standard output and standard error. */
+	fun exec(command: String): String = memScoped {
+		val stream = popen("$command 2>&1", "r") ?: fail("Command ($command) failed.")
+
+		return buildString {
+			val buffer = ByteArray(4096)
+			while (true) {
+				val input = fgets(buffer.refTo(0), buffer.size, stream) ?: break
+				append(input.toKString())
+			}
+
+			val status = pclose(stream)
+			if (status != 0) {
+				fail("Command ($command) failed with status: $status.")
+			}
+		}.trim()
+	}
+
+	/**
+	 *  Captures the existing values for all the mutable properties in [Environment] and restores them
+	 *  after [block] returns.
+	 */
+	fun withSnapshot(block: () -> Unit) {
+		val snapshot = snapshot()
+		try {
+			block()
+		} finally {
+			restore(snapshot)
+		}
+	}
+
+	private fun snapshot() = Snapshot(
+		workingDirectory = workingDirectory,
+		gitAuthorDate = gitAuthorDate,
+		gitAuthorEmail = gitAuthorEmail,
+		gitAuthorName = gitAuthorName,
+		gitCommitterDate = gitCommitterDate,
+		gitCommitterEmail = gitCommitterEmail,
+		gitCommitterName = gitCommitterName,
+	)
+
+	private fun restore(snapshot: Snapshot) {
+		workingDirectory = snapshot.workingDirectory
+		gitAuthorDate = snapshot.gitAuthorDate
+		gitAuthorEmail = snapshot.gitAuthorEmail
+		gitAuthorName = snapshot.gitAuthorName
+		gitCommitterDate = snapshot.gitCommitterDate
+		gitCommitterEmail = snapshot.gitCommitterEmail
+		gitCommitterName = snapshot.gitCommitterName
+	}
+
+	private class Variable(private val name: String) {
+		operator fun getValue(thisRef: Any?, property: KProperty<*>): String? {
+			return memScoped { getenv(name)?.toKString() }
+		}
+
+		operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String?) {
+			if (value == null) {
+				unsetenv(name)
+			} else {
+				setenv(name, value, 1)
+			}
+		}
+	}
+
+	private class Snapshot(
+		val workingDirectory: String,
+		val gitAuthorName: String?,
+		val gitCommitterName: String?,
+		val gitAuthorEmail: String?,
+		val gitCommitterEmail: String?,
+		val gitAuthorDate: String?,
+		val gitCommitterDate: String?,
+	)
 }
 
 /** Creates a temporary folder and transforms all operations to be relative to this folder. */
