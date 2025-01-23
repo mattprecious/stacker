c9c732643879bd174e262c65e361612a7e1e0219
Matthew Precious <mattprecious@gmail.com>
2025-01-23 00:34:47 -0500

    Fill out tests for RepoInit

--- src/commonTest/kotlin/com/mattprecious/stacker/test/git.kt
+++ src/commonTest/kotlin/com/mattprecious/stacker/test/git.kt
@@ -15,9 +15,28 @@ fun TestEnvironment.gitAdd(
 
 fun TestEnvironment.gitCommit(
 	message: String,
-) {
+): String {
 	// TODO: Escaping.
-	environment.exec("git commit -m \"$message\"")
+	environment.exec("git commit --allow-empty -m \"$message\"")
+	return gitHeadSha()
+}
+
+fun TestEnvironment.gitCheckoutBranch(
+	name: String,
+) {
+	environment.exec("git checkout $name")
+}
+
+fun TestEnvironment.gitCreateAndCheckoutBranch(
+	name: String,
+) {
+	environment.exec("git checkout -b $name")
+}
+
+fun TestEnvironment.gitSetDefaultBranch(
+	name: String,
+) {
+	environment.exec("git config set init.defaultBranch '$name'")
 }
 
 fun TestEnvironment.gitHeadSha(): String {
