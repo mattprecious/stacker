a4d2de103c918e45c343ad82fe0dff076ec2e33c
Matthew Precious <mattprecious@gmail.com>
2025-01-23 00:34:47 -0500

    Improve test APIs
    Asserting against statics and outputs separately hides a lot of weird
    behaviors that can lead to incorrect/flakey tests.
    
    Also, hook into the internal state machine to avoid races when the
    command switches internally.

--- src/commonMain/kotlin/com/mattprecious/stacker/command/StackerCommandScope.kt
+++ src/commonMain/kotlin/com/mattprecious/stacker/command/StackerCommandScope.kt
@@ -1,20 +1,21 @@
 package com.mattprecious.stacker.command
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.snapshotFlow
 import com.jakewharton.mosaic.text.AnnotatedString
 import com.jakewharton.mosaic.text.buildAnnotatedString
+import com.mattprecious.stacker.command.StackerCommand.WorkState
 import com.mattprecious.stacker.config.ConfigManager
 import com.mattprecious.stacker.lock.Locker
 import com.mattprecious.stacker.rendering.Printer
 import com.mattprecious.stacker.rendering.code
 import kotlinx.coroutines.awaitCancellation
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.first
 
 class StackerCommandScope internal constructor(
 	private val printer: Printer,
-	private val state: MutableStateFlow<StackerCommand.State>,
+	private val workState: WorkState,
 ) {
 	fun printStatic(message: String) {
 		printer.printStatic(message)
@@ -35,10 +36,14 @@ class StackerCommandScope internal constructor(
 	}
 
 	suspend fun <R> render(content: @Composable (onResult: (R) -> Unit) -> Unit): R {
-		state.value = StackerCommand.State.Rendering(content)
-		return state.filterIsInstance<StackerCommand.State.DeliveringRenderResult<R>>().first().result.also {
-			state.value = StackerCommand.State.Working
-		}
+		workState.state = StackerCommand.State.Rendering(content)
+		return snapshotFlow { workState.state }
+			.filterIsInstance<StackerCommand.State.DeliveringRenderResult<R>>()
+			.first()
+			.result
+			.also {
+				workState.state = StackerCommand.State.Working
+			}
 	}
 
 	suspend fun requireInitialized(configManager: ConfigManager) {
@@ -70,8 +75,8 @@ class StackerCommandScope internal constructor(
 	}
 
 	suspend fun abort(): Nothing {
-		require(state.value !is StackerCommand.State.TerminalState)
-		state.value = StackerCommand.State.Aborted
+		require(workState.state !is StackerCommand.State.TerminalState)
+		workState.state = StackerCommand.State.Aborted
 		awaitCancellation()
 	}
 }
--- src/commonTest/kotlin/com/mattprecious/stacker/test/RepoInitTest.kt
+++ src/commonTest/kotlin/com/mattprecious/stacker/test/RepoInitTest.kt
@@ -1,8 +1,6 @@
 package com.mattprecious.stacker.test
 
 import assertk.assertThat
-import assertk.assertions.isEmpty
-import assertk.assertions.isEqualTo
 import assertk.assertions.isFalse
 import com.mattprecious.stacker.command.repo.repoInit
 import kotlin.test.Test
@@ -13,13 +11,12 @@ class RepoInitTest {
 		gitInit()
 
 		testCommand({ repoInit() }) {
+			awaitFrame(
+				static = "Stacker cannot be initialized in a completely empty repository. " +
+					"Please make a commit first.",
+				output = "",
+			)
 			assertThat(awaitResult()).isFalse()
-			assertThat(awaitOutput()).isEmpty()
-			assertThat(awaitStatic())
-				.isEqualTo(
-					"Stacker cannot be initialized in a completely empty repository. " +
-						"Please make a commit first.",
-				)
 		}
 	}
 }
