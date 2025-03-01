0c738c467575d40d8b7a0364f1f5a2c8741743fe
Matthew Precious <mattprecious@gmail.com>
2025-02-28 21:53:22 -0500

    Add BranchRenameTest

---
+++ src/commonTest/kotlin/com/mattprecious/stacker/test/command/BranchRenameTest.kt
@@ -0,0 +1,108 @@
+package com.mattprecious.stacker.test.command
+
+import assertk.assertThat
+import assertk.assertions.containsExactly
+import assertk.assertions.containsExactlyInAnyOrder
+import assertk.assertions.isTrue
+import com.mattprecious.stacker.command.branch.branchCreate
+import com.mattprecious.stacker.command.branch.branchRename
+import com.mattprecious.stacker.command.repo.repoInit
+import com.mattprecious.stacker.db.Branch
+import com.mattprecious.stacker.delegates.Optional
+import com.mattprecious.stacker.test.util.gitBranches
+import com.mattprecious.stacker.test.util.gitCheckoutBranch
+import com.mattprecious.stacker.test.util.gitCommit
+import com.mattprecious.stacker.test.util.gitInit
+import com.mattprecious.stacker.test.util.withTestEnvironment
+import kotlin.test.Test
+
+class BranchRenameTest {
+	@Test
+	fun renameLeaf() = withTestEnvironment {
+		gitInit()
+		gitCommit("Empty")
+		testCommand({ repoInit("main", Optional.None) })
+		testCommand({ branchCreate("change-a") })
+
+		testCommand({ branchRename("new-a") }) {
+			awaitFrame("")
+			assertThat(awaitResult()).isTrue()
+		}
+
+		withDatabase {
+			assertThat(it.branchQueries.selectAll().executeAsList().map { it.name })
+				.containsExactly("main", "new-a")
+		}
+
+		assertThat(gitBranches()).containsExactly(
+			"main",
+			"* new-a",
+		)
+	}
+
+	@Test
+	fun renameWithChildren() = withTestEnvironment {
+		gitInit()
+		val sha = gitCommit("Empty")
+		testCommand({ repoInit("main", Optional.None) })
+		testCommand({ branchCreate("change-a") })
+		testCommand({ branchCreate("change-b") })
+		testCommand({ branchCreate("change-c") })
+		gitCheckoutBranch("change-a")
+		testCommand({ branchCreate("change-d") })
+		gitCheckoutBranch("change-a")
+
+		testCommand({ branchRename("new-a") }) {
+			awaitFrame("")
+			assertThat(awaitResult()).isTrue()
+		}
+
+		withDatabase {
+			assertThat(it.branchQueries.selectAll().executeAsList()).containsExactlyInAnyOrder(
+				Branch(
+					name = "main",
+					parent = null,
+					parentSha = null,
+					prNumber = null,
+					hasAskedToDelete = false,
+				),
+				Branch(
+					name = "new-a",
+					parent = "main",
+					parentSha = sha.long,
+					prNumber = null,
+					hasAskedToDelete = false,
+				),
+				Branch(
+					name = "change-b",
+					parent = "new-a",
+					parentSha = sha.long,
+					prNumber = null,
+					hasAskedToDelete = false,
+				),
+				Branch(
+					name = "change-c",
+					parent = "change-b",
+					parentSha = sha.long,
+					prNumber = null,
+					hasAskedToDelete = false,
+				),
+				Branch(
+					name = "change-d",
+					parent = "new-a",
+					parentSha = sha.long,
+					prNumber = null,
+					hasAskedToDelete = false,
+				),
+			)
+		}
+
+		assertThat(gitBranches()).containsExactly(
+			"change-b",
+			"change-c",
+			"change-d",
+			"main",
+			"* new-a",
+		)
+	}
+}
